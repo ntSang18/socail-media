766c259744a01edfba1cfc318379c7a764c261ef
ThanhSang <thanhsang6325@gmail.com>
2022-11-12 20:31:13 +0700

    mongo config

--- config/db/index.js
+++ config/db/index.js
@@ -2,9 +2,13 @@ const mongoose = require("mongoose");
 
 async function connect(url) {
 	try {
-		await mongoose.connect(url);
+		await mongoose.connect(url, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
 		console.log("Connect successfully!!!");
 	} catch (error) {
+		console.log(error);
 		console.log("Connect failure!!!");
 	}
 }
